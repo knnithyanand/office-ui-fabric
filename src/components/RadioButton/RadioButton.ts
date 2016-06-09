ff6469cecc0badbe447cc07e2a6f97c5b8ef9709
Phil Worthington <phwort@microsoft.com>
2016-06-08 17:02:22 -0700

    Reimplement radiobutton extending checkbox

--- src/components/RadioButton/RadioButton.ts
+++ src/components/RadioButton/RadioButton.ts
@@ -9,10 +9,7 @@ namespace fabric {
    * Adds basic demonstration functionality to .ms-RadioButton components.
    *
    */
-  export class RadioButton {
-
-    protected _choiceField: HTMLElement;
-    private _container: HTMLElement;
+  export class RadioButton extends CheckBox {
 
     /**
      *
@@ -20,46 +17,17 @@ namespace fabric {
      * @constructor
      */
     constructor(container: HTMLElement) {
-      this._container = container;
-      this._choiceField = <HTMLElement>this._container.querySelector(".ms-ChoiceField-field");
-      if (this._choiceField.getAttribute("aria-checked") === "true") {
-          this._choiceField.classList.add("is-checked");
-      }
-      this._addListeners();
-    }
-
-    public getValue(): boolean {
-      return this._choiceField.getAttribute("aria-checked") === "true" ? true : false;
-    }
-
-    public toggle(): void {
-      if (this.getValue()) {
-        this.unCheck();
-      } else {
-        this.check();
-      }
-    }
-
-    public check(): void {
-      this._choiceField.setAttribute("aria-checked", "true");
-      this._choiceField.classList.add("is-checked");
-    }
-
-    public unCheck(): void {
-      this._choiceField.setAttribute("aria-checked", "false");
-      this._choiceField.classList.remove("is-checked");
+      super(container);
     }
 
     public removeListeners(): void {
-      this._choiceField.removeEventListener("focus", this._FocusHandler.bind(this));
-      this._choiceField.removeEventListener("blur", this._BlurHandler.bind(this));
+      super.removeListeners();
       this._choiceField.removeEventListener("click", this._RadioClickHandler.bind(this));
       this._choiceField.addEventListener("keydown", this._RadioKeydownHandler.bind(this));
     }
 
     protected _addListeners(): void {
-      this._choiceField.addEventListener("focus", this._FocusHandler.bind(this), false);
-      this._choiceField.addEventListener("blur", this._BlurHandler.bind(this), false);
+      super._addListeners({ignore: ["keydown", "click"]});
       this._choiceField.addEventListener("click", this._RadioClickHandler.bind(this), false);
       this._choiceField.addEventListener("keydown", this._RadioKeydownHandler.bind(this), false);
     }
@@ -90,14 +58,6 @@ namespace fabric {
             this._dispatchSelectEvent();
         }
       }
-    }
-
-    private _FocusHandler(): void {
-      this._choiceField.classList.add("in-focus");
-    }
-
-    private _BlurHandler(): void {
-      this._choiceField.classList.remove("in-focus");
     }
   }
 }
